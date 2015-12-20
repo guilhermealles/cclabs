8eeee13d4a25034e680e1795506c58b65017d69b
ippossebon <Isadora@Isadoras-MacBook-Pro.local>
2015-12-20 16:58:29 +0100

    sdadada

--- week4/code_generation.c
+++ week4/code_generation.c
@@ -1,18 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "code_generation.h"
+#include "nfa.h"
 
-int final_states[];
-char* actions[];
-char* tokens[];
+dfa dfas[3];
+char actions[3][30];
+char tokens[3][30];
 
-void createDFATable(dfa dfa, FILE *file){
+void createDFATable(unsigned int dfa_index, FILE *file){
+    dfa dfa = dfas[dfa_index];
     int state, symbol;
-    printf("Numero de estados: %d \n", dfa.nstates);
     for (state = 0; state < dfa.nstates; state++){
         fprintf(file, "{");
         for (symbol = 0; symbol < 129; symbol++){
-            printf("%d\n", symbol);
             intSet state_transition = copyIntSet(dfa.transition[state][symbol]);
             // If the transition with the current symbol doesn't exists, writes -1. Otherwise, writes the target state.
             if (isEmptyIntSet(state_transition)){
@@ -42,12 +42,14 @@ void createDFATable(dfa dfa, FILE *file){
     }
 }
 
-void addDFADeclaration(dfa dfa, unsigned int dfa_count, FILE *file){
-    fprintf(file, "static int dfa_table%d[%d][128] = ", dfa_count, dfa.nstates);
-    createDFATable(dfa, file);
+void addDFADeclaration(unsigned int dfa_index, FILE *file){
+    dfa dfa = dfas[dfa_index];
+    fprintf(file, "static int dfa_table%d[%d][128] = ", dfa_index, dfa.nstates);
+    createDFATable(dfa_index, file);
 }
 
-void addFinalStatesDeclaration(dfa dfa, unsigned int dfa_count){
+void addFinalStatesDeclaration(unsigned int dfa_index, FILE* file){
+    dfa dfa = dfas[dfa_index];
     intSet final_states_copy = copyIntSet(dfa.final);
     int final_states_count = 0;
 
@@ -57,15 +59,16 @@ void addFinalStatesDeclaration(dfa dfa, unsigned int dfa_count){
         final_states_count++;
     }
 
-    fpintf(file, "int final_states_dfa%d[%d] = {", dfa_count, final_states_count);
+    fprintf(file, "int final_states_dfa%d[%d] = {", dfa_index, final_states_count);
     final_states_copy = copyIntSet(dfa.final);
 
     int i = 1;
+    printf("Dfa %d: final states count: %d\n", dfa_index, final_states_count);
     while (!isEmptyIntSet(final_states_copy)) {
         int state = chooseFromIntSet(final_states_copy);
         deleteIntSet(state, &final_states_copy);
         if (i == final_states_count){
-            fprintf(file, "%d};\n");
+            fprintf(file, "%d};\n", state);
         }else{
             fprintf(file, "%d, ", state);
             i++;
@@ -74,11 +77,17 @@ void addFinalStatesDeclaration(dfa dfa, unsigned int dfa_count){
     }
 }
 
+void addTokenDeclaration(unsigned int dfa_index, FILE *file){
+    fprintf(file, "char* token_dfa%d = \"%s\";\n", dfa_index, tokens[dfa_index]);
+}
 
+void addActionsDeclaration(unsigned int dfa_index, FILE *file){
+    fprintf(file, "char* action_dfa%d = \"%s\";\n", dfa_index, actions[dfa_index]);
+}
 
-void addItem(dfa dfa, unsigned int dfa_count, FILE *file){
-    addDFADeclaration(dfa, dfa_count, file);
-    addFinalStatesDeclaration(dfa, dfa_count, file);
-    // add tokens
-    // add actions
+void addItem(unsigned int dfa_index, FILE *file){
+    addDFADeclaration(dfa_index, file);
+    addFinalStatesDeclaration(dfa_index, file);
+    addTokenDeclaration(dfa_index, file);
+    addActionsDeclaration(dfa_index, file);
 }
